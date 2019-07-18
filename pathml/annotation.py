81159c12e6ff6256665a4fce48d474e83c529a8b
Marcel Gehrung <marcel.gehrung@cruk.cam.ac.uk>
2019-07-18 11:15:31 +0100

    Restructure folders

---
+++ pathml/annotation.py
@@ -0,0 +1,12 @@
+import pyvips as pv
+
+class Annotation:
+
+    __verbosePrefix = '[PathML] '
+
+    def __init__(self, parentSlide, maskingMode='inverse', verbose=False):
+        # maskingMode: 'inverse' sets all areas to zero / ''
+        pass
+
+
+    # packing mode / and other modes for tiling
