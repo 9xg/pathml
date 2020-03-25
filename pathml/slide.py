d285d073e10c164ab45a76128e03e992e77fe11b
Unknown <marcel.gehrung@cruk.cam.ac.uk>
2020-03-25 16:30:44 +0000

    Dynamic sourcing of cwd

--- pathml/slide.py
+++ pathml/slide.py
@@ -182,7 +182,7 @@ class Slide:
         else:
             return np.unravel_index(tileIndex, (self.numTilesInX, self.numTilesInY), order='F')
 
-    def saveTile(self, tileAddress, fileName, folder='./'):
+    def saveTile(self, tileAddress, fileName, folder=os.getcwd()):
         if not hasattr(self, 'tileMetadata'):
             raise PermissionError(
                 'setTileProperties must be called before accessing tiles')
