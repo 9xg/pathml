cfbc882e46b9d1bfe5a980b394652ec128c2b89f
Marcel Gehrung <marcel.gehrung@cruk.cam.ac.uk>
2021-05-02 18:21:14 +0100

    Cleaning up master branch

--- pathml/models/tissuedetector.py
+++ pathml/models/tissuedetector.py
@@ -15,7 +15,7 @@ def tissueDetector():
     model_ft = models.densenet121(pretrained=False)
     model_ft.classifier = torch.nn.Linear(1024, 3)
     # It might struggle finding this dict here. Use full path to pathml
-    model_ft.load_state_dict(torch.load('/Users/berman01/pathml/pathml/models/deep-tissue-detector_densenet_state-dict.pt',map_location=torch.device('cpu')))
+    model_ft.load_state_dict(torch.load('../pathml/models/deep-tissue-detector_densenet_state-dict.pt',map_location=torch.device('cpu')))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if torch.cuda.device_count() > 1:
         model_ft = torch.nn.DataParallel(model_ft)
